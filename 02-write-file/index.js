8a18ebed4618173a918c2c8cf66c554186bf8a6b
Maksim Hal. <gitmasterai@tutamail.com>
2026-08-08 05:41:48 +0300

    02-write-file
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js
    
    Update index.js

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,1 +1,23 @@
+const path = require('node:path');
+const fs = require('node:fs');
+const process = require('node:process');
+const readline = require('node:readline');
 
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath, { encoding: 'utf8' });
+const readlineInterface = readline.createInterface({ 
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Please, enter your text below:');
+
+readlineInterface.on('line', (input) => {  
+  if (input === 'exit') {
+    readlineInterface.close();
+  } else {
+    writeStream.write(`${input}\n`);
+  }
+});
+
+readlineInterface.on('close', () => console.log('Thank you for checking!'));
